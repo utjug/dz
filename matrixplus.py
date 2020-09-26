a250eb31a7d7f12e8eb4c6eadd699c6733164bb0
utjug <70849209+utjug@users.noreply.github.com>
2020-09-26 20:51:34 +0300

    Add files via upload
    сумма матриц на 30.09

---
+++ matrixplus.py
@@ -0,0 +1,23 @@
+a=[]
+for i in range(3):
+	a.append([])
+	for j in range(3):
+	    a[i].append(int(input("ch")))
+print("a=",a)
+
+b=[]
+for g in range(3):
+	b.append([])
+	for k in range(3):
+	    b[g].append(int(input("ch")))
+print("b=",b)
+
+c=[]
+for l in range(3):
+	c.append([])
+	for n in range(3):
+		c[l].append(0)
+for i in range(3):
+	for j in range(3):
+		c[i][j]=a[i][j]+b[i][j]
+print("c=",c)
